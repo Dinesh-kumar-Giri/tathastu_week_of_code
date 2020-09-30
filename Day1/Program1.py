7f64a6068c17b05457b17ae3feff9713626df8be
Dinesh Kumar Giri <66900440+Dinesh-kumar-Giri@users.noreply.github.com>
2020-10-01 00:52:21 +0530

    Improvement Code

--- Day1/Program1.py
+++ Day1/Program1.py
@@ -1,11 +1,12 @@
 #Program 1 of Tathastu-Week of Code by shaswat-99
-n1=input("Enter your name")
-n2=input("Enter your Branch")
-n3=input("Enter your Gender")
-n4=input("Enter your College name")
+print("\n****************************************************************\n");
+n1=input("Enter your name \n");
+n2=input("Enter your Branch \n");
+n3=input("Enter your Gender\n");
+n4=input("Enter your College name \n");
 n5=int(input("Enter your Age"))
-print("Name = ",n1)
-print("Branch = ",n2)
-print("Gender = ",n3)
-print("College = ",n4)
-print("Age = ",n5)
+print("Name = ",n1);
+print("Branch = ",n2);
+print("Gender = ",n3);
+print("College = ",n4);
+print("Age = ",n5);
